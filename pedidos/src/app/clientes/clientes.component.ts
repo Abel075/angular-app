4ef685d3057a19e8e49e39445c17414a1077290e
Abel075 <abeld.correa@gmail.com>
2021-05-31 14:27:54 -0300

    localstorage

--- pedidos/src/app/clientes/clientes.component.ts
+++ pedidos/src/app/clientes/clientes.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Clientes {
+  nombre: string;
+  apellido:string;
+  edad: number;
+
+}
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -7,9 +14,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
+  clientes: Array<Clientes> = new Array<Clientes>();
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.clientes.push(
+    //   {
+    //   nombre: "Carmen",
+    //   apellido: "Najera",
+    //   edad: 34
+    //  },
+    //   {
+    //   nombre: "Alejando",
+    //   apellido: "Peineda",
+    //   edad: 18
+    //  }
+
+    )
+  }
+
+  guardarCliente()
+  {
+    localStorage.setItem('clientes', JSON.stringify(this.clientes))
+  }
+
+  leerClientes()
+  {
+    // leer como texto
+    // console.log(localStorage.getItem('clientes'));
+
+    // console.log(JSON.parse(localStorage.getItem("clientes")))
   }
 
 }
